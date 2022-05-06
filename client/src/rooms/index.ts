7b8ac66e9db31586fb62fd182e3b7c6ec7a85266
JaviGalindo <javi.galindo.1991@hotmail.com>
2022-05-06 20:51:23 +0100

    Eslint fix

--- client/src/rooms/index.ts
+++ client/src/rooms/index.ts
@@ -1,9 +1,10 @@
 import RoomIcon from '@mui/icons-material/Comment';
 import RoomCreate from './RoomCreate';
 import RoomList from './RoomList';
-
-export default {
+const Room =  {
     icon: RoomIcon,
     list: RoomList,
     create: RoomCreate,
 };
+
+export default Room
