814ff86f6b4ac72e78e9ececc0fe64b2eeacb0b5
JaviGalindo <javi.galindo.1991@hotmail.com>
2022-05-08 19:42:02 +0100

    server test (not working)

---
+++ myapp/tests/server.test.js
@@ -0,0 +1,14 @@
+//const request = require("supertest");
+//const server = require("../src/server");
+
+// DB needs to be mock :(
+
+describe("Devices", () => {
+	it("TRUE", async () =>{
+		/*const response = await request(server).get("/devices")
+			.set("Accept", "application/json");
+		expect(response.status).toEqual(200);
+		expect(JSON.parse(response.text).length).toEqual(8);*/
+		expect(true).toBeTruthy();
+	});
+});
