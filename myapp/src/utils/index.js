3ed24e3d423042b8249c97fe92d0de39da727970
JaviGalindo <javi.galindo.1991@hotmail.com>
2022-05-08 19:41:03 +0100

    new functions in utils

--- myapp/src/utils/index.js
+++ myapp/src/utils/index.js
@@ -1,7 +1,20 @@
-
 const getResourceNameFromPath = (path) => path.replace("/", "");
-const pepe = "";
+
+const returnInsertSintaxFromObj = (table, JSONObject) => {
+	let values = "";
+	const columns = Object.keys(JSONObject);
+	columns.forEach(() => values += "?,");
+	values = `(${values.slice(0,-1)})`; //remove last comma
+	const insertTxt = `INSERT INTO ${table} (${columns.join(",")}) VALUES ${values}`;
+	return insertTxt;
+};
+
+const returnUpdateSintaxFromObj = (table, JSONObject) => {
+	return `UPDATE ${table} SET ${Object.keys(JSONObject).join("=?,")}=? WHERE id=?`;
+};
 
 module.exports = {
-	getResourceNameFromPath
+	getResourceNameFromPath,
+	returnInsertSintaxFromObj,
+	returnUpdateSintaxFromObj
 };
