6d6378626f9861f8838bd660de8e77cbbd345898
JaviGalindo <javi.galindo.1991@hotmail.com>
2022-05-08 19:39:02 +0100

    adding bodyparser

--- myapp/src/server.js
+++ myapp/src/server.js
@@ -1,11 +1,16 @@
 const { APPPort } = require("../config/config");
 const express = require("express");
+const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 const mysqlConnection = require("./databases/mysql");
 
 const {devices, rooms } = require("./routes");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(
 	"/api-docs",
 	swaggerUi.serve, 
@@ -20,3 +25,5 @@ app.listen(APPPort, async() => {
 	await mysqlConnection();
 }
 );
+
+module.exports = app;
