67e8f04c11ec3141c215f4448900c0b539c58e85
JaviGalindo <javi.galindo.1991@hotmail.com>
2022-05-08 13:48:49 +0100

    change env configuration

--- myapp/config/config.js
+++ myapp/config/config.js
@@ -1,4 +1,12 @@
+/* eslint-disable no-undef */
 module.exports = {
-    port: process.env.NODE_DOCKER_PORT,
-  };
+	"APPPort": process.env.SERVER_PORT,
+	"mysql" : {
+		"host": process.env.MYSQLDB_HOST,
+		"user": process.env.MYSQLDB_USER,
+		"password": process.env.MYSQLDB_ROOT_PASSWORD,
+		"database": process.env.MYSQLDB_DATABASE,
+		"port": process.env.MYSQLDB_LOCAL_PORT
+	}
+};
   
