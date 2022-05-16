48f59152c2e7d75280fee79b47176a5c64e01d20
JaviGalindo <javi.galindo.1991@hotmail.com>
2022-05-16 18:13:29 +0100

    Making API working with DB in within Docker

--- myapp/config/config.js
+++ myapp/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
 		"user": process.env.MYSQLDB_USER,
 		"password": process.env.MYSQLDB_ROOT_PASSWORD,
 		"database": process.env.MYSQLDB_DATABASE,
-		"port": process.env.MYSQLDB_LOCAL_PORT
+		"port": process.env.MYSQLDB_PORT
 	}
 };
   
